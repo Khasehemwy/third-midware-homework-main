f5750d16727b9acefb89010bb4348ef096baa669
卡塞凯姆威 <BuluyuAzir@foxmail.com>
2022-05-11 11:46:02 +0800

    Add files via upload

---
+++ src/com/gsy/client/ProduceClient.java
@@ -0,0 +1,9 @@
+package com.gsy.client;
+
+import java.io.IOException;
+
+public class ProduceClient {
+    public static void main(String[] args) throws IOException {
+        MqClient.produce("Hello World");
+    }
+}
